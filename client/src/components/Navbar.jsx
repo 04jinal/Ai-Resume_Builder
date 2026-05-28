39f28c07e21ea074244cdde4317c028313ec6af6
04jinal <152520742+04jinal@users.noreply.github.com>
2026-05-28 10:39:37 +0530

    client

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Link, useNavigate } from 'react-router-dom'
+import React from 'react'
+import Logo from '../assets/logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../app/features/authSlice';
+
+const Navbar = () => {
+    // TODO: Use real user data from auth context or state
+    const{ user }= useSelector(state => state.auth)
+    const dispatch = useDispatch()
+
+    const navigate = useNavigate();
+
+    const logoutUser = () => {
+        navigate('/');
+        dispatch(logout())
+    };
+
+    return (
+        <div className='shadow bg-white'>
+            <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-4 text-slate-800 transition-all'>
+                <Link to='/'>
+                    <img src={Logo} alt="logo" className="h-11 w-auto" />
+                </Link>
+                <div className='flex items-center gap-4 text-sm'> 
+                    <p className='max-sm:hidden'>Hi, {user?.name}</p>
+                    <button onClick={logoutUser} className='bg-white hover:bg-slate-50 border border-gray-300 px-7 py-2 rounded-full active:scale-95 transition-all'>Logout</button>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
